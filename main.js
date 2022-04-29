3ef57a2767d4d72f2ee207d96d9bed59a1c06df8
Jiafu Chen <jiafu.chen.7e5@itb.cat>
2022-04-29 02:07:56 +0200

    some

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 let estiljs = () => {
     document.getElementById("text").style.color = "red"
 }
+
+let prenValorForm = () => {
+    event.preventDefault()
+    let form = document.getElementById("formulari1")
+    console.log(form["nom"].value,form["cognom"].value)
+}
+
+let ex3 = () =>{
+   let oPs = document.querySelectorAll("p")
+   oPs.forEach(item => {
+       item.style.background = "green"
+   })
+}
+
+let obtenirAtributs = () =>{
+    let attributs = document.getElementById("itb").attributes
+    console.log(attributs)
+}
